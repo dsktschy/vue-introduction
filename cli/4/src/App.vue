96a0d3894045e4b9d972d10daa4511ae8954eac6
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-08-17 14:41:15 +0900

    Step-4 of CLI

---
+++ cli/4/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <app-modal :visible="appModalVisible"></app-modal>
+  </div>
+</template>
+
+<script>
+import AppModal from './components/app-modal.vue'
+
+export default {
+  components: {
+    AppModal: AppModal
+  },
+  data: function () {
+    return {
+      appModalVisible: false
+    }
+  }
+}
+</script>
+
+<style>
+*, ::before, ::after { margin: 0; box-sizing: border-box; }
+</style>
---
+++ cli/4/src/components/app-modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="app-modal" v-show="visible">
+    <div class="image">
+      <p class="text">
+        {{text}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'visible'
+  ],
+  data: function () {
+    return {
+      text: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, .7);
+}
+.image {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+  width: 80vw;
+  height: 80vh;
+  background-size: cover;
+  background-position: center;
+  background-image: url(/images/image-1.jpg);
+}
+.text {
+  position: absolute;
+  bottom: 36px;
+  right: 40px;
+  line-height: 1.5;
+  font-size: 16px;
+  font-weight: 900;
+  text-align: right;
+  color: #fff;
+}
+</style>
