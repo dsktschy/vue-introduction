d5685fd29a1345096479e4548f72141e0aae13f2
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-08-17 12:49:01 +0900

    Step-2 of CLI

---
+++ cli/2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <app-text></app-text>
+    <app-text></app-text>
+    <app-text></app-text>
+  </div>
+</template>
+
+<script>
+import AppText from './components/app-text.vue'
+
+export default {
+  components: {
+    AppText: AppText
+  }
+}
+</script>
+
+<style>
+*, ::before, ::after { margin: 0; box-sizing: border-box; }
+</style>
---
+++ cli/2/src/components/app-text.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>{{text}}</p>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      text: 'Hello world!'
+    }
+  }
+}
+</script>
+
