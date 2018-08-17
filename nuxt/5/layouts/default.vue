65aceee231586da735e77b76b1578430d6ceb54b
dsktschy <chamestica@gmail.com>
2018-08-17 18:55:08 +0900

    Step-5 of Nuxt

---
+++ nuxt/5/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-common :image="createImageUrl()"/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import AppCommon from '~/components/app-common'
+
+export default {
+  components: {
+    AppCommon
+  },
+  methods: {
+    createImageUrl () {
+      const max = 3
+      const min = 1
+      const number = Math.floor(Math.random() * (max + 1 - min) + min)
+      return `/images/image-${number}.jpg`
+    }
+  }
+}
+</script>
