745a8a20324d786b4309819e708f00aa9349d56f
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-08-17 14:55:14 +0900

    Step-5 of CLI

---
+++ cli/5/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <button @click="openAppModal">Open</button>
+    <app-modal
+      :visible="appModalVisible"
+      @click-outside="closeAppModal"
+    ></app-modal>
+  </div>
+</template>
+
+<script>
+import AppModal from './components/app-modal.vue'
+
+export default {
+  components: {
+    AppModal: AppModal
+  },
+  data: function () {
+    return {
+      appModalVisible: false
+    }
+  },
+  methods: {
+    openAppModal: function () {
+      this.appModalVisible = true
+    },
+    closeAppModal: function () {
+      this.appModalVisible = false
+    }
+  }
+}
+</script>
+
+<style>
+*, ::before, ::after { margin: 0; box-sizing: border-box; }
+</style>
