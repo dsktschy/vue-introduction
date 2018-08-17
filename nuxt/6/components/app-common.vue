441630cffbd01ffb35f11e990ff51f6d4d1a6333
dsktschy <chamestica@gmail.com>
2018-08-17 19:20:08 +0900

    Step-6 of Nuxt

---
+++ nuxt/6/components/app-common.vue
@@ -0,0 +1,101 @@
+<template>
+  <div :class="[ 'app-common', { 'app-common-offset': $route.path !== '/' } ]">
+    <div
+      class="image"
+      :style="{ 'background-image': `url(${image})` }"
+    >
+      <div class="content">
+        <h1 class="heading">
+          <nuxt-link to="/">山田太郎・Taro YAMADA</nuxt-link>
+        </h1>
+        <nav>
+          <ul>
+            <li class="nav-item">
+              <nuxt-link to="/profile" class="nav-text">プロフィール・Profile</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="/works" class="nav-text">制作実績・Works</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to="/contact" class="nav-text">お問い合わせ・Contact</nuxt-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'image'
+  ]
+}
+</script>
+
+<style scoped>
+.app-common {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  padding: 40px;
+  transition: transform .6s cubic-bezier(0.075, 0.82, 0.165, 1);
+}
+.app-common-offset {
+  transform: translate3d(-280px, 0, 0);
+}
+.image {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+}
+.content {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  width: 100%;
+  padding-right: 40px;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -30px;
+    right: 180px;
+    display: block;
+    width: 80px;
+    height: 80px;
+    background-color: rgba(255, 0, 0, .2);
+    animation: rotation 10s cubic-bezier(0.075, 0.82, 0.165, 1) 0s infinite;
+  }
+}
+.heading {
+  position: relative;
+  z-index: 1;
+  height: 40px;
+  line-height: 1;
+  font-size: 28px;
+  font-weight: 100;
+  text-align: right;
+  color: #fff;
+  border-bottom: 1px solid #fff;
+}
+.nav-item {
+  margin-top: 11px;
+  line-height: 1;
+  font-size: 16px;
+  font-weight: 100;
+  color: #fff;
+  text-align: right;
+}
+.nav-text {
+  &:hover,
+  &.nuxt-link-active {
+    font-weight: 900;
+  }
+}
+</style>
---
+++ nuxt/6/pages/works.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="page">
+    <ul class="page-inner works">
+      <li
+        class="works-item"
+        :style="{ 'background-image': `url(/images/image-1.jpg)` }"
+      >
+        <pre class="works-text">Work-1</pre>
+      </li>
+      <li
+        class="works-item"
+        :style="{ 'background-image': `url(/images/image-2.jpg)` }"
+      >
+        <pre class="works-text">Work-2</pre>
+      </li>
+      <li
+        class="works-item"
+        :style="{ 'background-image': `url(/images/image-3.jpg)` }"
+      >
+        <pre class="works-text">Work-3</pre>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.works-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 112px;
+  background-size: cover;
+  background-position: center;
+  margin-left: auto;
+  margin-right: 0;
+  cursor: pointer;
+  &:not(:first-child) {
+    margin-top: 30px;
+  }
+}
+.works-text {
+  line-height: 1.5;
+  font-size: 12px;
+  font-weight: 900;
+  text-align: center;
+  color: #fff;
+}
+</style>
