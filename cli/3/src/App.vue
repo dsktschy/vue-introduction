a254b50372b7a681642908a2aa8d5408e59bcc75
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-08-17 13:05:01 +0900

    Step-3 of CLI

---
+++ cli/3/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <app-modal></app-modal>
+  </div>
+</template>
+
+<script>
+import AppModal from './components/app-modal.vue'
+
+export default {
+  components: {
+    AppModal: AppModal
+  }
+}
+</script>
+
+<style>
+*, ::before, ::after { margin: 0; box-sizing: border-box; }
+</style>
