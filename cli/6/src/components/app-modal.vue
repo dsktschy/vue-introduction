4a544540483b575006a4d55573969a2d1342c8d8
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-08-17 15:12:41 +0900

    Step-6 of CLI

---
+++ cli/6/src/components/app-modal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="app-modal"
+    v-show="visible"
+    @click.self="$emit('click-outside')"
+  >
+    <div
+      class="image"
+      :style="{ 'background-image': `url(${image})` }"
+    >
+      <p class="text">
+        {{text}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'visible',
+    'image',
+    'text'
+  ]
+}
+</script>
+
+<style scoped>
+.app-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, .7);
+}
+.image {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+  width: 80vw;
+  height: 80vh;
+  background-size: cover;
+  background-position: center;
+}
+.text {
+  position: absolute;
+  bottom: 36px;
+  right: 40px;
+  line-height: 1.5;
+  font-size: 16px;
+  font-weight: 900;
+  text-align: right;
+  color: #fff;
+}
+</style>
