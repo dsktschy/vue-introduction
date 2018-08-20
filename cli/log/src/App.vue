64563f7e29a331de884e9e3f37f1630c36993f6f
dsktschy <chamestica@gmail.com>
2018-08-20 19:10:35 +0900

    CLI/Step-2 for checking diff

--- cli/log/src/App.vue
+++ cli/log/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-text></app-text>
+    <app-text></app-text>
+    <app-text></app-text>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppText from './components/app-text.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    AppText: AppText
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+*, ::before, ::after { margin: 0; box-sizing: border-box; }
 </style>
